18657e02397e9a4022d0745ee19bc0544ce190ae
sud0woodo <40278342+sud0woodo@users.noreply.github.com>
2022-08-31 17:23:53 +0200

    Update dissect.cstruct version (#3)
    (DIS-1441)

--- setup.py
+++ setup.py
@@ -4,6 +4,6 @@ setup(
     name="dissect.clfs",
     packages=["dissect.clfs"],
     install_requires=[
-        "dissect.cstruct>=2.0.dev,<3.0.dev",
+        "dissect.cstruct>=3.0.dev,<4.0.dev",
     ],
 )
